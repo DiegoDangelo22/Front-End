858c00b1d0b93931b6be57f7e1e4beab9ce94f89
Kaxzark <dangelo.diego.gabriel@gmail.com>
2023-02-26 19:27:25 -0300

    Profile pictures' image cropping & bug fixes

--- src/app/components/about/edit-about.component.ts
+++ src/app/components/about/edit-about.component.ts
@@ -4,6 +4,8 @@ import { Persona } from 'src/app/model/persona.model';
 import { ImageService } from 'src/app/services/image.service';
 import { PersonaService } from 'src/app/services/persona.service';
 import { TokenService } from 'src/app/services/token.service';
+import { getStorage, Storage, ref, uploadBytes, list, getDownloadURL, uploadBytesResumable } from '@angular/fire/storage';
+import Cropper from 'cropperjs';
 
 @Component({
   selector: 'app-edit-about',
@@ -12,7 +14,8 @@ import { TokenService } from 'src/app/services/token.service';
 })
 export class EditAboutComponent implements OnInit {
   persona: Persona = null;
-  constructor(public persoServ: PersonaService, private tokenService: TokenService, private activatedRoute:ActivatedRoute, private router:Router, public imgServ:ImageService) {  }
+  url:string = "";
+  constructor(public persoServ: PersonaService, private tokenService: TokenService, private activatedRoute:ActivatedRoute, private router:Router, public imgServ:ImageService, private storage:Storage) {  }
   isLogged = false;
   isAdmin = false;
   ngOnInit(): void {
@@ -31,16 +34,23 @@ export class EditAboutComponent implements OnInit {
     if(this.router.url == '/editpersona/1' && this.isAdmin == false) {
       this.router.navigate(['/login']);
     }
-  }
 
+
+   
+  }
+    
 
   onUpdate():void{
     const id = this.activatedRoute.snapshot.params['id'];
-    if(this.imgServ.url === ""){
-      this.persona.img;
-    } else {
-      this.persona.img = this.imgServ.url;
+    // if(this.imgServ.url === ""){
+    //   this.persona.img;
+    // } else if (this.imgServ.url !== "") {
+    //   this.persona.img = this.imgServ.url;
+    // }
+    if (this.url !== "") {
+      this.persona.img = this.url;
     }
+    
     this.persoServ.update(id, this.persona).subscribe(
       data => {
         this.router.navigate(['/portfolio']);
@@ -57,13 +67,87 @@ export class EditAboutComponent implements OnInit {
     })
   }
 
+  
+
   uploadImage($event:any){
+    
     const id = this.activatedRoute.snapshot.params['id'];
     const name = "perfil_" + id;
 
-    this.imgServ.uploadImage($event, name);
+    const file = $event.target.files[0]
+
+    
+      const imgRef = ref(this.storage, `img/` + name)
+      
+      uploadBytes(imgRef, file)
+    // .then(response => {this.getImages()})
+    .then(async () => {let asd = await getDownloadURL(imgRef)
+    this.url = asd;
+    let image:any = document.querySelector("#koog");
+    image.src = this.url;
+    let cropper = new Cropper(image, {
+      aspectRatio: 0,
+      viewMode: 1,
+      checkCrossOrigin: true,
+      checkOrientation: false,
+      crop(event) {
+      console.log(event.detail.x);
+      console.log(event.detail.y);
+      console.log(event.detail.width);
+      console.log(event.detail.height);
+      console.log(event.detail.rotate);
+      console.log(event.detail.scaleX);
+      console.log(event.detail.scaleY);
+    },
+    
+  })
+  document.querySelector("#cropImgBtn").addEventListener('click', ()=>{
+    let croppedImage = cropper.getCroppedCanvas().toDataURL("image/png");
+    let output:any = document.querySelector("#output");
+    let oo:any = document.querySelector("#myimg");
+    let momo:any = document.querySelector("#momo");
+  
+    output.src = croppedImage;
+    this.url = croppedImage;
+    console.log(this.url)
+    oo.src = croppedImage;
+    momo.src = croppedImage;
+    
+  })
+
+
+    console.log("La url es: " + this.url)
+    
+  })
+    .catch(error => console.log(error))
+    
     let imgvieja:HTMLImageElement = document.querySelector('#myimg');
     imgvieja.style.opacity = '0'
+    
+
+    var modalPP:HTMLElement = document.querySelector('.modalPP');
+
+    modalPP.style.display = "flex";
+    window.onclick = function(event) {
+      if (event.target == modalPP) {
+        modalPP.style.display = "none";
+      }
+    } 
+
+
+
+
+
+        
+        
+    
+
+        
+        
+        
   }
 
+  
+
+  
 }
